364e227d7dcd39f2c4ca6b2ba9e327c70581b658
bellmontsistema <bellmontsistema@gmail.com>
2021-05-22 08:30:22 -0300

    google tag managem tentativa 1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Router from 'next/router'
 import Head from 'next/head'
 import { NextSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import { GTMPageView } from '../utils/gtm'
 
 import theme from 'styles/theme'
 import GlobalStyle from 'styles/global.styles'
 import WhatsAppWidgets from 'components/WhatsAppWidgets'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  // Initiate GTM
+  useEffect(() => {
+    const handleRouteChange = (url: string) => GTMPageView(url)
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <Head>
